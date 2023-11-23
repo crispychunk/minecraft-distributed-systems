1ab94bf0fb1d77d8961ec78c178b145f6af2b674
Crispin <crispinliu168@gmail.com>
2023-11-22 16:17:50 -0800

    Add ip for main

--- distributed-server/src/main.ts
+++ distributed-server/src/main.ts
@@ -1,8 +1,27 @@
 import { DistributedServerNode, loadFromFile } from "./distributedNode/distributedNode";
+import { networkInterfaces } from "os";
+
+function getLocalIpAddress(): string | null {
+  const interfaces = networkInterfaces();
+  const addresses: string[] = [];
+
+  Object.keys(interfaces).forEach((ifname) => {
+    interfaces[ifname]?.forEach((iface) => {
+      if ("IPv4" === iface.family && !iface.internal) {
+        addresses.push(iface.address);
+      }
+    });
+  });
+
+  return addresses.length > 0 ? addresses[0] : null;
+}
+
 function main() {
   // Initialize networks
   // Find public IP
-  const address: string = "localhost";
+  const localIpAddress = getLocalIpAddress();
+  const address: string = localIpAddress;
+  console.log(localIpAddress);
   const httpPort: number = 8080;
   const rSyncPort: number = 8081;
   const minecraftPort: number = 8082;
